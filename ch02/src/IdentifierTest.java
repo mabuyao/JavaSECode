657e4e814b215afad48d816a176587e5e23d2eed
MaBuyao <860977475@qq.com>
2023-07-19 16:19:56 +0800

    ch02

---
+++ ch02/src/IdentifierTest.java
@@ -0,0 +1,19 @@
+/**
+ * ClassName: IdentifierTest
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Mabuyao
+ * @Create 2023/7/19 11:54
+ * @Version 1.0
+ */
+public class IdentifierTest {
+    public static void main(String[] args) {
+        //测试标识符的使用
+        int abc = 12;
+        int age = 12;
+
+        int myAge = 22;
+        System.out.println(myAge);
+    }
+}
---
+++ ch02/src/VariableTest.java
@@ -0,0 +1,20 @@
+/**
+ * ClassName: VariableTest
+ * Package: PACKAGE_NAME
+ * Description:测试变量的基本使用
+ *
+ * @Author Mabuyao
+ * @Create 2023/7/19 15:06
+ * @Version 1.0
+ */
+public class VariableTest {
+    public static void main(String[] args) {
+        int age = 10;
+
+        char gender;//变量的声明
+        gender = '男';//变量的赋值（初始化）
+
+        System.out.println(age);
+        System.out.println("age = " + age);
+    }
+}

e2474b4575455bf452f32b1d7337fbe1909f734d
MaBuyao <860977475@qq.com>
2023-08-28 09:16:36 +0800

    mby0828

---
+++ ch12_collection_map/src/com/xiaoma01/collection/CollectionTest.java
@@ -0,0 +1,24 @@
+package com.xiaoma01.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * ClassName: CollectionTest
+ * Description:
+ * Collection中的方法的使用
+ * @Author Mabuyao
+ * @Create 2023/8/26 14:01
+ * @Version 1.0
+ */
+public class CollectionTest {
+    @Test
+    public void test1(){
+        Collection coll = new ArrayList();
+
+        coll.
+
+    }
+}
